0df3a8077b9da756b5e2736b45abdf085d258084
Jijiri <jijirijohnson.com>
2026-05-16 15:04:47 +0000

    testing

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,11 +1,5 @@
 <div>
- 
-   
-
-    <livewire:home.about />
-
-    <livewire:home.mission />
-     <livewire:home.partnership />
+<livewire:home.partnership />
      <livewire:home.leadership />
      
 </div>
